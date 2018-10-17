e367d14ad6b33e2c35fb4316b543ab1e9f529a8b
Alexander Kennedy <rexer1998@gmail.com>
2018-10-16 21:52:58 -0700

    Implimented a stack to store course data
    in progress

---
+++ test.js
@@ -0,0 +1,9 @@
+var items={animal:"cat",human:"boi",sheepArray:[{sound:'baa',shape:'round'}]};
+var items2={animal:"dogo",human:"me"};
+
+var data=[];
+data.push(items);
+data.push(items2);
+
+console.log(data.pop());
+console.log(data.pop());
